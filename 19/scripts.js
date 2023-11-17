5656a27a2096e617dff7de3adf6ca6cfd1d897b1
Iuliia Ilina <yulencha@gmail.com>
2023-11-17 23:16:11 +0700

    Исправления для  19 задачи

--- 19/scripts.js
+++ 19/scripts.js
@@ -48,11 +48,12 @@ function fetchPostsFromVK() {
 function renderNewPosts(newPosts) {
   const postsHTML = newPosts
     .map(function (post) {
-      console.log(post);
+      // console.log(post);
       return `
       <li class="widget__post post">
         <div class="post__title">${post.text}</div>
         <div class="post__date">${new Date(post.date * 1000).toLocaleDateString()}</div>
+        <img class="post__img" src=${post.attachments[0]["photo"].sizes[4].url}
       </li>
     `;
     })
